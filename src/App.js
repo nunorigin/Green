68a36893a522b0901024a9976a24d736b3ed232b
BalajiM1603 <mbalaji1603@gmail.com>
2022-04-25 14:19:11 +0530

    comit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   console.log("jekkk")
   return (
     <>
-      <Router>
+      <Router> 
         <NavBar />
         <Routes>
           <Route exact path='/' element={<Home />} />
